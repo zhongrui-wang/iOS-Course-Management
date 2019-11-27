1a9e5144a3d34c6c0ba208e0f7339809ee3c3c74
Matheus Bustanante <matheusbustamante@live.com>
2019-11-27 08:19:07 -0600

    log in

---
+++ mgt100/LoginViewController.swift
@@ -0,0 +1,49 @@
+//
+//  LoginViewController.swift
+//  mgt100
+//
+//  Created by Matheus Bustamante on 11/26/19.
+//  Copyright © 2019 Innovation that excites. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class LoginViewController: UIViewController {
+    
+    
+    @IBOutlet weak var emailTextInput: UITextField!
+    @IBOutlet weak var passwordTextInput: UITextField!
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+        if let email = emailTextInput.text, let password = passwordTextInput.text {
+            Auth.auth().signIn(withEmail: email, password: password) { (authResult, error) in
+                if let e = error {
+                    print(e.localizedDescription)
+                } else {
+                    self.performSegue(withIdentifier: "LoginToChat", sender: self)
+                }
+            }
+        }
+        
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ mgt100/RegisterViewController.swift
@@ -0,0 +1,38 @@
+//
+//  RegisterViewController.swift
+//  mgt100
+//
+//  Created by Matheus Bustamante on 11/26/19.
+//  Copyright © 2019 Innovation that excites. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBAction func registerButton(_ sender: UIButton) {
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
+                if let e = error{
+                    print(e.localizedDescription)
+                } else {
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+            }
+        }
+       
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+
+}
